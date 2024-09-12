ed73d099fd87d9606228b519c847654cf0b279aa
BaryshovMichael <barushoff@gmail.com>
2024-09-12 22:44:12 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+Quant_of_finished_HM = 12
+Quant_of_spent_hours = 1.5
+Course_name          = 'Python'
+Time_for_one_tusk    = (Quant_of_spent_hours/Quant_of_finished_HM)
+print('Курс:',Course_name,', всего задач:',Quant_of_finished_HM,', затрачено часов:',Quant_of_spent_hours,', среднее время выполнения:',Time_for_one_tusk,' часов.')
