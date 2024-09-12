6a64a0307a6e60ed963bfa101b531412047c1d16
BaryshovMichael <barushoff@gmail.com>
2024-09-12 22:45:40 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,4 +2,4 @@ Quant_of_finished_HM = 12
 Quant_of_spent_hours = 1.5
 Course_name          = 'Python'
 Time_for_one_tusk    = (Quant_of_spent_hours/Quant_of_finished_HM)
-print('Курс:',Course_name,', всего задач:',Quant_of_finished_HM,', затрачено часов:',Quant_of_spent_hours,', среднее время выполнения:',Time_for_one_tusk,' часов.')
+print('Курс:',Course_name,', всего задач:',Quant_of_finished_HM,', затрачено часов:',Quant_of_spent_hours,', среднее время выполнения:',Time_for_one_tusk,' часа.')
