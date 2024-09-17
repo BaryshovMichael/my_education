e2dbd1250b5bbc5b2c79931e19086fc69791ef86
BaryshovMichael <barushoff@gmail.com>
2024-09-18 00:09:44 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -2,15 +2,13 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 positive_numbers = []
 start_variable = 0
 
-
 while start_variable < len(my_list):
-    if my_list[start_variable] <= 0:
-        start_variable += 1
-        continue
-    else:
+
+    if my_list[start_variable] > 0:
         positive_numbers.append(my_list[start_variable])
+        if start_variable < len(positive_numbers):
+            print(positive_numbers[start_variable])
+            start_variable += 1
+            continue
+    else:
         start_variable += 1
-    if start_variable == len(my_list):
-        print(positive_numbers)
-
-
