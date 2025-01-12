8ca2d4f4ebb6c030e27cf37bedc1f004621a7ffb
BaryshovMichael <barushoff@gmail.com>
2025-01-12 22:52:55 +0300

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -1,17 +1,15 @@
 import os, time
-from importlib.metadata import files
+from os.path import join,getmtime,getsize,dirname
 
 print('Текущая директория: ', os.getcwd())
 
 directory = '.'
 for root,dirs,files in os.walk(directory):
     for file in files:
-        f=file
-        filepath = os.path.join(root,f)
-        filetime = os.path.getmtime(f)
+        filepath = os.path.join(root,file)
+        filetime = os.path.getmtime(file)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(f)
-        parent_dir = os.path.dirname(os.path.abspath(f))
-        print(f'Обнаружен файл: {f}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
-    break
+        filesize = os.path.getsize(file)
+        parent_dir = os.path.dirname(os.path.abspath(file))
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
 
